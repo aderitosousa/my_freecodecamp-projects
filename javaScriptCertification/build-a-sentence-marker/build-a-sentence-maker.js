5aecf778c2ec30bfd2a838061793c491878d66b1
aderitosousa <aderitodourado155@gmail.com>
2026-04-10 15:45:26 +0100

    add new dir aboute JS and file

---
+++ javaScriptCertification/build-a-sentence-marker/build-a-sentence-maker.js
@@ -0,0 +1,25 @@
+
+   let adjective = " Beatful"
+   ,noun = " Orquideas flor"
+   ,verb = " Go"
+   ,place = " ELYSEUS FIELDS"
+    ,adjective2 = " calm"
+    ,noun2 = " sky" ;
+
+    let firstStory;
+
+    firstStory= "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+    console.log("First story: " + firstStory);
+
+adjective = "Resplendent"
+   ,noun = "face"
+   ,verb = "look"
+   ,place = "in my heart"
+    ,adjective2 = "confortable"
+    ,noun2 = "cinderela" ;
+
+    let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+    console.log("Second story: "+ secondStory)
+
+
